c601f8d6d548c042cb3c6f436dce014c5b967e8b
Abiscula <victorabiscula@live.com>
2022-05-27 16:26:59 -0300

    refatorado arquivo de rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import { DataUrl } from './Controller/DataUrl'
+import { ScrapRoutes } from './Controller/ScrapRoutes'
 
 export const routes = express.Router()
 
-routes.post('/bigboygames', DataUrl.getBigBoyUrl)
+routes.post('/bigboygames', ScrapRoutes.getBigBoyUrl)
