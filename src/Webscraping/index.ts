16199bd99f5c3134d69a08a42e213882e0f66a24
Abiscula <victorabiscula@live.com>
2022-05-27 16:26:43 -0300

    adicionado resolução da tela para facilitar a screenshoot

--- src/Webscraping/index.ts
+++ src/Webscraping/index.ts
@@ -1,34 +1,26 @@
-import  puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer'
+import { ScrapLocation } from '../Controller/ScrapLocation'
 
 const gameUrl = 'https://www.bigboygames.com.br/mario-kart-8-deluxe-switch-2912-p989541'
+const sites = new ScrapLocation()
 
 async function configuration() {
     if(!gameUrl) {
         console.log('Aguardando URL para busca de preço')
         return
     }
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(gameUrl);
-
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setViewport({width: 1366, height: 768})
+    await page.goto(gameUrl)
     return page
-};
-
-
-async function bigBoyGames(page: any) {
-
-    const price = await page.evaluate(() => {
-        let priceSpan: any = document.querySelector("#info_preco")
-        let Children = priceSpan?.children[1].innerText
-        return Children
-    })
-    console.log(price)
 }
 
+
 export async function startTracking() {
-    const page = await configuration();
+    const page = await configuration()
     
     setInterval(() => {
-        bigBoyGames(page);
+        sites.bigBoyGames(page)
     }, 30000)
 }
