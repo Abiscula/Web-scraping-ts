aec02ee32a1d92eb2faee1106ac20a1fae5e9651
Victor <victorabiscula@live.com>
2022-05-31 01:44:30 -0300

    adicionado setInterval para intercalar as requisições aos sites

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,9 @@ app.use(express.json())
 app.use(routes)
 
 
-startTracking()
+setInterval(() => {
+    startTracking()
+}, 30000)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
