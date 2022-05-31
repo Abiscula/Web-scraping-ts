f33486c166bb8d453a6142a2fb9617a9643e008b
Victor <victorabiscula@live.com>
2022-05-31 01:44:02 -0300

    alterado lógica para chamada da função, adicionado chamada do metodo Amazon

--- src/Webscraping/index.ts
+++ src/Webscraping/index.ts
@@ -1,26 +1,21 @@
 import puppeteer from 'puppeteer'
 import { ScrapLocation } from '../Controller/ScrapLocation'
 
-const gameUrl = 'https://www.bigboygames.com.br/mario-kart-8-deluxe-switch-2912-p989541'
-const sites = new ScrapLocation()
+const bigBoyURL = 'https://www.bigboygames.com.br/mario-kart-8-deluxe-switch-2912-p989541'
+const amazonURL = 'https://www.amazon.com.br/Mario-Kart-8-Deluxe-Switch/dp/B01N1037CV/ref=sr_1_1?keywords=mario+kart+8&qid=1653966926&sprefix=%2Caps%2C164&sr=8-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147'
 
-async function configuration() {
-    if(!gameUrl) {
-        console.log('Aguardando URL para busca de preço')
-        return
-    }
+const scrapLocation = new ScrapLocation()
+
+async function configuration(game_url: string) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.setViewport({width: 1366, height: 768})
-    await page.goto(gameUrl)
+    await page.goto(game_url)
     return page
 }
 
 
 export async function startTracking() {
-    const page = await configuration()
-    
-    setInterval(() => {
-        sites.bigBoyGames(page)
-    }, 30000)
+    scrapLocation.bigBoyGames(await configuration(bigBoyURL))
+    scrapLocation.Amazon(await configuration(amazonURL))
 }
