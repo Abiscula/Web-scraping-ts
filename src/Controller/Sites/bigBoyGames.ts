ab0e6d4b472fdecc80038ea9ca1904db7fc09d81
Victor <victorabiscula@live.com>
2022-06-01 16:51:14 -0300

    recuperado gamePrice do db e adicionado validação para produto fora de estoque

--- src/Controller/Sites/bigBoyGames.ts
+++ src/Controller/Sites/bigBoyGames.ts
@@ -1,14 +1,21 @@
+import { Sites } from "../../db/model/Sites"
 import Email from "../../utils/Email"
 
 export async function BigBoyGames(page: any) {
+    const { dataValues:  { gamePrice } }: any = await Sites.findOne({ where: {site: 'BigBoyGames'}})
 
     const price = await page.evaluate(() => {
         let priceSpan: any = document.querySelector("#info_preco")
         let children = priceSpan?.children[1].innerText
-        children = children.slice(3).replace(',', '.')
-        return Number(children)
+        if(children) {
+            children = children.slice(3).replace(',', '.')
+            return Number(children)
+        } else {
+            children = 999.99
+            return children
+        }
     })
-    if(price < 280) {
+    if(price <= gamePrice) {
         const screenshot = await page.screenshot({
             encoding: 'base64',
         })
