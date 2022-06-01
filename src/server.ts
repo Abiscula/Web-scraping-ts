d5df73c029986196d9633fc4c673d3934ce50853
Victor <victorabiscula@live.com>
2022-05-31 23:34:44 -0300

    alterado chamada do scrap

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { routes } from './routes'
-import { startTracking } from './Webscraping'
+import { startScraping } from './Webscraping'
 
 const app = express()
 const port = 3333
@@ -8,10 +8,7 @@ const port = 3333
 app.use(express.json())
 app.use(routes)
 
-
-setInterval(() => {
-    startTracking()
-}, 30000)
+startScraping()
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
