4ca96cb2844b59e39268dd50491a18a4f5a008af
Victor <victorabiscula@live.com>
2022-06-01 17:21:22 -0300

    Criado validação para rota

--- src/Controller/ScrapRoutes.ts
+++ src/Controller/ScrapRoutes.ts
@@ -7,19 +7,22 @@ export class ScrapRoutes {
     static async gameToSearch(req: Request, res: Response) {
         const { site, game_name, game_url, game_price } = req.body
 
-        const db = await Sites.findAll({ where: {site: site}})
-        if(db.length === 0) {
-            await Sites.create({ site: site, gameName: game_name,  gameUrl: game_url, gamePrice: game_price })
-        } else {
-            await Sites.update(
-                { gameName: game_name,  gameUrl: game_url, gamePrice: game_price },
-                { where: { site: site } })
+        try {
+            const db = await Sites.findAll({ where: {site: site}})
+            if(db.length === 0) {
+                await Sites.create({ site: site, gameName: game_name,  gameUrl: game_url, gamePrice: game_price })
+            } else {
+                await Sites.update(
+                    { gameName: game_name,  gameUrl: game_url, gamePrice: game_price },
+                    { where: { site: site } })
+            }
+
+            return res.status(200).json(db)
+        }catch(err) {
+            console.log(err)
+            return res.status(400).json(err)
         }
 
-        const selectAll: any = await Sites.findAll()
-        console.log(selectAll.forEach((data: any) => console.log(data['dataValues'])))
-
-        return res.status(200).send(`${game_name} adicionado ao alerta de preços`)
     }
 
 }
