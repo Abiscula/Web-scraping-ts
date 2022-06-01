d9fe465220781d5e5edca2504e7828c650e6b9c0
Victor <victorabiscula@live.com>
2022-06-01 16:51:05 -0300

    recuperado gamePrice do db e adicionado validação para produto fora de estoque

--- src/Controller/Sites/Amazon.ts
+++ src/Controller/Sites/Amazon.ts
@@ -1,15 +1,23 @@
+import { Sites } from "../../db/model/Sites"
 import Email from "../../utils/Email"
 
 export async function Amazon(page: any) {
-
+    const { dataValues:  { gamePrice } }: any = await Sites.findOne({ where: {site: 'Amazon'}})
+    
     const price = await page.evaluate(() => {
         const XPath = '//div[@id="corePrice_feature_div"]/div/span'
         const priceSpan: any = document.evaluate(XPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
         let children = priceSpan.innerText
-        children = children.slice(2, 8).replace(',', '.')
-        return Number(children)
+        if(children) {
+            children = children.slice(2, 8).replace(',', '.')
+            return Number(children)
+        } else {
+            children = 999.99
+            return children
+        }
     })
-    if(price < 280) {
+
+    if(price <= gamePrice) {
         const screenshot = await page.screenshot({
             encoding: 'base64',
         })
