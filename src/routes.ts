87bac2f1f44eefd176890e4ae50ac7547fdfd91f
Victor <victorabiscula@live.com>
2022-06-01 01:59:19 -0300

    ajuste na rota

--- src/routes.ts
+++ src/routes.ts
@@ -3,4 +3,4 @@ import { ScrapRoutes } from './Controller/ScrapRoutes'
 
 export const routes = express.Router()
 
-routes.post('/bigboygames', ScrapRoutes.gameToSearch)
+routes.post('/game4buy', ScrapRoutes.gameToSearch)
