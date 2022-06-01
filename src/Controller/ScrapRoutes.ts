2551b94a30487b93c649e45540f3429ac32446cf
Victor <victorabiscula@live.com>
2022-05-31 22:51:59 -0300

    adicionado novo parametro a ser recebido pela requisição

--- src/Controller/ScrapRoutes.ts
+++ src/Controller/ScrapRoutes.ts
@@ -2,10 +2,12 @@ import { Request, Response } from 'express';
 
 export class ScrapRoutes {
 
-    static getBigBoyUrl(req: Request, res: Response) {
-        const { game_name, game_url, game_price } = req.body
+    static gameToSearch(req: Request, res: Response) {
+        const { site, game_name, game_url, game_price } = req.body
 
         //criar lógica para salvar url no banco de dados
+        //site deve ser definido como PK
+        
 
         return res.status(200).send(`${game_name} adicionado ao alerta de preços`)
     }
