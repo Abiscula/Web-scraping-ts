ce28c14274957665eafa32e463eb20d3ca516203
Victor <victorabiscula@live.com>
2022-06-01 01:59:07 -0300

    implementado lógica para salvamento de dados no db

--- src/Controller/ScrapRoutes.ts
+++ src/Controller/ScrapRoutes.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from 'express';
+import { Sites } from '../db/model/Sites';
+
 
 export class ScrapRoutes {
 
-    static gameToSearch(req: Request, res: Response) {
+    static async gameToSearch(req: Request, res: Response) {
         const { site, game_name, game_url, game_price } = req.body
 
-        //criar lógica para salvar url no banco de dados
-        //site deve ser definido como PK
-        
+        const db = await Sites.findAll({ where: {site: site}})
+        if(db.length === 0) {
+            await Sites.create({ site: site, gameName: game_name,  gameUrl: game_url, gamePrice: game_price })
+        } else {
+            await Sites.update(
+                { gameName: game_name,  gameUrl: game_url, gamePrice: game_price },
+                { where: { site: site } })
+        }
+
+        const selectAll: any = await Sites.findAll()
+        console.log(selectAll.forEach((data: any) => console.log(data['dataValues'])))
 
         return res.status(200).send(`${game_name} adicionado ao alerta de preços`)
     }
