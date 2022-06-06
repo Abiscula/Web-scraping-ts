6472cf0aa42334e931f3217020c8b41e8119be1a
Victor <victorabiscula@live.com>
2022-06-05 23:16:59 -0300

    alterado chamada para inicialização do db

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,5 +2,17 @@ import { Sequelize } from 'sequelize'
 
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: 'src/db/games.sqlite'
+  storage: 'src/db/games.sqlite',
 })
+
+export async function startDB() {
+  try {
+    await sequelize.sync()
+    console.log('Database is connected')
+    
+    return 200
+
+  }catch(err) {
+    console.log(err)
+  }
+}
